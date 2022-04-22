703b13e4807d512b400c0b5e00a196ed561fd723
A-Ghacker <62658251+A-Ghacker@users.noreply.github.com>
2022-04-23 01:51:15 +0200

    Todo Application 1st version

---
+++ src/components/TodoForm.js
@@ -0,0 +1,56 @@
+import React,{useState,useEffect,useRef} from 'react'
+
+function TodoForm(props) {
+    const [input,setInput] = useState(props.edit ? props.edit.value : '');
+    const inputRef = useRef(null)
+    useEffect(() => {
+        inputRef.current.focus()
+    }) 
+
+
+    const handleChange = e =>{
+        setInput(e.target.value);
+    };
+
+    //stoping refresh on submit 
+    const handleSubmit = e =>{
+        e.preventDefault();
+        props.onSubmit({
+            id : Math.floor(Math.random()*1000),
+            text:input
+        });
+        setInput('');
+    }
+    
+    return (
+    <div>
+        <form className='todo-form' onSubmit={handleSubmit}>
+            {props.edit ? ( 
+            <>
+            <input type="text" placeholder='Update your item' 
+                value={input} 
+                name="text"
+                className='todo-input edit'
+                onChange={handleChange}
+                ref={inputRef}
+            ></input>
+            <button className='todo-button edit'>Update</button> </>
+            ) 
+            :
+            <><input type="text" placeholder='Add a todo' 
+            value={input} 
+            name="text"
+            className='todo-input'
+            onChange={handleChange}
+            ref={inputRef}
+        ></input>
+        <button className='todo-button'>Add</button> </>
+        }
+            
+               
+        </form>
+    </div>
+  )
+}
+
+export default TodoForm
